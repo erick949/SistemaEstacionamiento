07c02668e02044582234e2e5c4c625a1d6b36482
Erick Miranda Gil <erickmiranda954@gmail.com>
2025-06-11 10:00:43 -0600

    Editar DOkerfile

--- client/src/api/persona.js
+++ client/src/api/persona.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:8000/persona/'; // ajusta la URL si es necesario
-const API_BASE_URL = `${import.meta.env.VITE_API_URL}persona/`;
+const API_URL = `${import.meta.env.VITE_API_URL}persona/`;
 
 const handleResponse = async (response) => {
   if (response.ok) {
--- server/server/settings.py
+++ server/server/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url  # Agrega esta importación arriba
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +96,33 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgres://usuario:contraseña@host:puerto/nombre_db',
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 
 
 # Password validation
